7376dc83b4a7c00c80f59157893a08a8a0d1fa50
Jonathan Chng <jonathanchngyk@gmail.com>
2023-03-14 21:19:26 +0800

    first commit

--- main.py
+++ main.py
@@ -13,4 +13,35 @@ TWITTER_PASSWORD = os.environ['TWITTER_PASSWORD']
 
 up = 50
 down = 150
-driver = webdriver.Chrome(chromedriver_path)
+
+class InternetSpeed:
+
+    def __init__(self, up, down):
+        self.driver = webdriver.Chrome(chromedriver_path)
+        self.link = "https://www.speedtest.net/"
+        self.download = down
+        self.upload = up
+
+    def get_internet_speed(self):
+        self.driver.get(self.link)
+        time.sleep(5)
+
+        start = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a')
+        start.click()
+
+        time.sleep(60)
+
+        download = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+        upload = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+
+        print(f"Download: {download}\nUpload: {upload}")
+
+
+
+    def tweet_at_provider(self):
+        pass
+
+isn = InternetSpeed(150,50)
+
+
+isn.get_internet_speed()
