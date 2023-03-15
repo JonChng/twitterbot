5e311dcf38bb72f2659591af051378a5fdb5475a
Jonathan Chng <jonathanchngyk@gmail.com>
2023-03-15 17:13:35 +0800

    Tweet

--- main.py
+++ main.py
@@ -34,11 +34,11 @@ class InternetSpeed:
         download = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
         upload = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
 
-        print(f"Download: {download}\nUpload: {upload}")
+        return download, upload
 
 
 
-    def tweet_at_provider(self):
+    def tweet_at_provider(self, down, up):
 
         self.driver.get("https://twitter.com/?lang=en")
         x_path = '//*[@id="layers"]/div/div[1]/div/div/div/div/div/div/div/div[1]/a'
@@ -62,8 +62,8 @@ class InternetSpeed:
 
         time.sleep(30)
 
-        tweet = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
-        tweet.send_keys("HELLO!")
+        tweet = self.driver.find_element(By.XPATH, "//div[@data-testid='tweetTextarea_0']/div/div/div/span")
+        tweet.send_keys(f"Download: {down}\n Upload: {up}")
 
         btn = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]')
         btn.click()
@@ -78,5 +78,5 @@ class InternetSpeed:
 isn = InternetSpeed(150,50)
 
 
-# isn.get_internet_speed()
-isn.tweet_at_provider()
+down, up = isn.get_internet_speed()
+isn.tweet_at_provider(down, up)
