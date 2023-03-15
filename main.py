79531101da08efa994b376ff78fdfeb69b7ba7e8
Jonathan Chng <jonathanchngyk@gmail.com>
2023-03-15 16:56:09 +0800

    Login

--- main.py
+++ main.py
@@ -39,9 +39,44 @@ class InternetSpeed:
 
 
     def tweet_at_provider(self):
-        pass
+
+        self.driver.get("https://twitter.com/?lang=en")
+        x_path = '//*[@id="layers"]/div/div[1]/div/div/div/div/div/div/div/div[1]/a'
+
+        time.sleep(25)
+
+        login = self.driver.find_element(By.XPATH, x_path)
+        login.click()
+
+        time.sleep(3)
+
+        username = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
+        username.send_keys(TWITTER_EMAIL)
+        username.send_keys(Keys.ENTER)
+
+        time.sleep(25)
+
+        password_ = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+        password_.send_keys(TWITTER_PASSWORD)
+        password_.send_keys(Keys.ENTER)
+
+        time.sleep(30)
+
+        tweet = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
+        tweet.send_keys("HELLO!")
+
+        btn = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]')
+        btn.click()
+
+        time.sleep(25)
+
+
+
+
+
 
 isn = InternetSpeed(150,50)
 
 
-isn.get_internet_speed()
+# isn.get_internet_speed()
+isn.tweet_at_provider()
